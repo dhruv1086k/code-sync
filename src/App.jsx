53d169cb23f0bf7066144cc2a3fc2758d1e06d6c
Dhruv <dhruvpal.0905@gmail.com>
2025-10-05 13:00:21 +0530

    add toaster for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
+      <div>
+        {/* Toaster for notifications */}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#4aed88",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { toast, Toaster } from "react-hot-toast";
 
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const uid = uuidv4();
+    setRoomId(uid);
+    toast.success("New room created");
+  };
+
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <div className="bg-[#282a36] p-4 rounded-2xl w-[400px] max-sm:w-11/12">
@@ -11,18 +23,26 @@ const Home = () => {
             type="text"
             placeholder="ROOM ID"
             className="bg-white mt-5 p-1 rounded-sm outline-none"
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)} // if user copy paste id then store it in state var
           />
           <input
             type="text"
             placeholder="USERNAME"
             className="bg-white mt-5 p-1 rounded-sm outline-none"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
           />
-          <button className="bg-[#4aee88] px-10 self-end mt-2 py-1 rounded-sm font-semibold">
+          <button className="bg-[#4aee88] hover:bg-[#2eba64] px-10 cursor-pointer transition-all duration-250 ease-in-out self-end mt-2 py-1 rounded-sm font-semibold">
             Join
           </button>
           <span className="text-white text-sm text-center mt-5">
             If you don't have an invite then create &nbsp;
-            <a href="#" className="text-[#4aee88] underline">
+            <a
+              href="#"
+              onClick={createNewRoom}
+              className="text-[#4aee88] border-b-2"
+            >
               new room
             </a>
           </span>
